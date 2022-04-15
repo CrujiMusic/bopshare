109cf9faac04803d5cf681d3a30448d067ea8c55
Jesse Riley <jriley32@student.gsu.edu>
2022-04-15 19:52:47 -0400

    Connect to Spotify API

--- bopshareapp/app/src/main/java/com/example/bopshareapp/MainActivity.kt
+++ bopshareapp/app/src/main/java/com/example/bopshareapp/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.parse.ParseObject
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.client.Subscription
+import com.spotify.protocol.types.PlayerState
+import com.spotify.protocol.types.Track
 
 class MainActivity : AppCompatActivity() {
+
+    private val CLIENT_ID = "ce0aa4e253dc42b8a3ec6a9ed8ff0c1b"
+    private val REDIRECT_URI = "bop-share-app-login://callback"
+    private var spotifyAppRemote:SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,15 +31,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    protected override fun onStart() {
+    override fun onStart() {
         super.onStart()
+
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object: Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+            }
+        })
     }
 
     private fun connected() {
-
+        spotifyAppRemote!!.playerApi.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+        spotifyAppRemote!!.playerApi.subscribeToPlayerState().setEventCallback {
+            val track: Track = it.track
+            Log.d("MainActivity", track.name + " by " + track.artist.name)
+        }
     }
 
-    protected override fun onStop() {
+    override fun onStop() {
         super.onStop()
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
     }
 }
